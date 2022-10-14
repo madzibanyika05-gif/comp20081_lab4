dcd48395da978030646078bdb39b0097a5375d1f
ntu-user <ntu-user@10272e708db8>
2022-10-14 19:19:25 +0000

    first commit

---
+++ lab4/src/main/java/com/mycompany/lab4/Dog.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lab4;
+
+import java.util.*;
+
+/**
+ *
+ * @author ntu-user
+ */
+public class Dog {
+    // TODO: create private attrbutes
+    
+    // TODO: Create public methods
+    
+
+    public Dog(){
+        System.out.println("A Dog object created with success");
+    }
+  
+    
+}
---
+++ lab4/src/main/java/com/mycompany/lab4/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lab4;
+
+/**
+ *
+ * @author ntu-user
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Dog Dog1=new Dog();
+        // TODO handle the Object Dog1 here
+    }
+}
